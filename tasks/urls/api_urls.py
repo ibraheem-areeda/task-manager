aaab9b840978a9e6ba82721ee92e6c61d84c3d1f
ibraheem-areeda <ibraheem.areeda@gmail.com>
2025-02-03 20:03:11 +0300

    implementation task list template and some fixes

--- tasks/urls/api_urls.py
+++ tasks/urls/api_urls.py
@@ -3,6 +3,6 @@ from ..views.api_views import ListCreateTasks, TaskDetailView
 
 
 urlpatterns = [
-    path("api/tasks/", ListCreateTasks.as_view(), name="tasks list"),
-    path('api/tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path("api/tasks/", ListCreateTasks.as_view(), name="api tasks list"),
+    path('api/tasks/<int:pk>/', TaskDetailView.as_view(), name='api task-detail'),
 ]
