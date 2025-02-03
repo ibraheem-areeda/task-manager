957ca76b3c9fadaedd32a65aa9e70fc0d111e088
ibraheem-areeda <ibraheem.areeda@gmail.com>
2025-02-03 21:51:34 +0300

    - create form for user registration - for tasks urls (delete urls.py no need for that file) - add some basic styles for the form - add app name in urls and use it in the templates to be more clear

--- tasks/urls/__init__.py
+++ tasks/urls/__init__.py
@@ -1,5 +1,5 @@
 from .web_urls import urlpatterns as web_patterns
 from .api_urls import urlpatterns as api_patterns
 
-
+app_name = 'tasks'
 urlpatterns = web_patterns + api_patterns
--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
+from django.contrib.auth import login
 
 def user_register(request):
-    print("User register view")
-    return render(request, "user_register.html")
-
-
-
-
+    if request.method == "POST": 
+        form = UserCreationForm(request.POST) 
+        if form.is_valid(): 
+            login(request, form.save())
+            return redirect("tasks:tasks list")
+    else:
+        form = UserCreationForm()
+    return render(request, "user_register.html", { "form": form })
