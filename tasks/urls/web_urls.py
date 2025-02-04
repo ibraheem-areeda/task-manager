afde89eabe3731903cdec3deade8959a3e942a03
ibraheem-areeda <ibraheem.areeda@gmail.com>
2025-02-04 22:40:26 +0300

    - create task details view & template - access the task title in the tasks list

--- tasks/urls/web_urls.py
+++ tasks/urls/web_urls.py
@@ -5,4 +5,5 @@ from ..views import web_views
 urlpatterns = [
     path("tasks-list/", web_views.tasks_list, name="tasks list"),
     path("tasks-form/", web_views.tasks_form, name="tasks form"),
+    path("tasks-details/<int:id>/", web_views.tasks_details, name="tasks details"),
 ]
--- tasks/views/web_views.py
+++ tasks/views/web_views.py
@@ -7,6 +7,10 @@ def tasks_list(request):
     tasks = Task.objects.all().order_by("-created_at")
     return render(request, "task_list.html", {"tasks": tasks})
 
+def tasks_details(request,id):
+    task = Task.objects.get(id=id)
+    return render(request, "tasks_details.html", {"task": task})
+
 def tasks_form(request):
     if request.method == "POST":
         form = CreateTaskForm(request.POST)
