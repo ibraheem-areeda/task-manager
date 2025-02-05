a55e692f94acbe5e3c978137a2ffb1eb51ff109b
ibraheem-areeda <ibraheem.areeda@gmail.com>
2025-02-05 05:14:04 +0300

    - add completed to create task form

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,4 +4,4 @@ from .models import Task
 class CreateTaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description','user']
+        fields = ['title', 'description','user','completed']
